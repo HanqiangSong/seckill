7f3d5cbfa5a79762e733c0768568640070421535
HanqiangSong <hanqiangsong1024@gmail.com>
2020-12-28 16:13:11 +0800

    Add MyBatis to deal with db and add test

--- src/main/java/com/countdown/seckill/SeckillApplication.java
+++ src/main/java/com/countdown/seckill/SeckillApplication.java
@@ -1,9 +1,13 @@
 package com.countdown.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.countdown.seckill.db.mappers")
+@ComponentScan(basePackages = {"com.countdown"})
 public class SeckillApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/countdown/seckill/db/dao/SeckillCommodityDao.java
@@ -0,0 +1,8 @@
+package com.countdown.seckill.db.dao;
+
+import com.countdown.seckill.db.po.SeckillCommodity;
+
+public interface SeckillCommodityDao {
+
+    public SeckillCommodity querySeckillCommodityById(long commodityId);
+}
